09e8cc7a9ac1eee031cb440ed1b9934a3c659edf
Jakub Gmur <jakubgmur@Op.pl>
2020-04-18 00:56:32 +0200

    added routing

--- ZUT-PokerPlanningClient/src/app/project-planning/project-planning-routing.module.ts
+++ ZUT-PokerPlanningClient/src/app/project-planning/project-planning-routing.module.ts
@@ -2,8 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ProjectPlanningComponent } from './project-planning.component';
+import { GamesComponent } from './games/games.component';
+import { MembersComponent } from './members/members.component';
+import { SettingsComponent } from './settings/settings.component';
 
-const routes: Routes = [{ path: '', component: ProjectPlanningComponent }];
+const routes: Routes = [
+  {path: 'project-planning', component: ProjectPlanningComponent},
+  {path: 'games', component: GamesComponent},
+  {path: 'members', component: MembersComponent},
+  {path: 'settings', component: SettingsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
