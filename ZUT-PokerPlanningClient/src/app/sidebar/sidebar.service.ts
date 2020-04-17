041441d42e4251ec94cbd876b2b593026be186f5
Jakub Gmur <jakubgmur@Op.pl>
2020-04-17 19:20:48 +0200

    1. Built SideBarComponent. 2. Added boostrap, bsdatepicker, fortawesome, perfectscrollbar,

---
+++ ZUT-PokerPlanningClient/src/app/sidebar/sidebar.service.ts
@@ -0,0 +1,119 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SidebarService {
+  toggled = false;
+  _hasBackgroundImage = true;
+  menus = [
+    {
+      title: 'General',
+      type: 'header'
+    },
+    {
+      title: 'Projects',
+      icon: 'fa fa-tachometer-alt',
+      active: false,
+      type: 'dropdown',
+      badge: {
+        text: 'New ',
+        class: 'badge-warning'
+      },
+      submenus: [
+        {
+          title: 'Project AAAAA',
+          badge: {
+            text: 'Pro ',
+            class: 'badge-success'
+          }
+        },
+        {
+          title: 'Project BBBB'
+        },
+        {
+          title: 'Project CCCC'
+        }
+      ]
+    },
+    {
+      title: 'Chat',
+      icon: 'fa fa-globe',
+      active: false,
+      type: 'simple,',
+      badge: {
+        text: 'Beta',
+        class: 'badge-primary'
+      },
+    },
+    {
+      title: 'Friends',
+      icon: 'fa fa-user',
+      active: false,
+      type: 'simple,',
+      badge: {
+        text: 'Beta',
+        class: 'badge-primary'
+      },
+    },
+    {
+      title: 'Others',
+      type: 'header'
+    },
+    {
+      title: 'History',
+      icon: 'fa fa-book',
+      active: false,
+      type: 'simple',
+      badge: {
+        text: 'Beta',
+        class: 'badge-primary'
+      },
+    },
+    {
+      title: 'Import/Export',
+      icon: 'fa fa-calendar',
+      active: false,
+      type: 'simple',
+      badge: {
+        text: 'Beta',
+        class: 'badge-primary'
+      },
+    },
+    {
+      title: 'Introduction',
+      icon: 'fa fa-gem',
+      active: false,
+      type: 'simple',
+      badge: {
+        text: 'Beta',
+        class: 'badge-primary'
+      },
+    }
+  ];
+  constructor() { }
+
+  toggle() {
+    this.toggled = ! this.toggled;
+  }
+
+  getSidebarState() {
+    return this.toggled;
+  }
+
+  setSidebarState(state: boolean) {
+    this.toggled = state;
+  }
+
+  getMenuList() {
+    return this.menus;
+  }
+
+  get hasBackgroundImage() {
+    return this._hasBackgroundImage;
+  }
+
+  set hasBackgroundImage(hasBackgroundImage) {
+    this._hasBackgroundImage = hasBackgroundImage;
+  }
+}
